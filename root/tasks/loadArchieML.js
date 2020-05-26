c2c63765ea06301efed83caa307993ddd317ddbb
Gabrielle LaMarr LeMee <lamarrlemee@gmail.com>
2020-05-26 10:20:56 -0400

    use regular archieml and add data to js folder

--- root/tasks/loadArchieML.js
+++ root/tasks/loadArchieML.js
@@ -6,24 +6,21 @@ Anything that has a .txt extension in /data will be loaded
 */
 
 var path = require("path");
-var betty = require("@nprapps/betty");
+var archieml = require("archieml");
 
 module.exports = function(grunt) {
 
-  grunt.registerTask("archieml", "Loads ArchieML files from data/*.txt", function() {
+  grunt.registerTask("archieml", "Loads ArchieML files from src/js/data/*.txt", function() {
 
     grunt.task.requires("state");
     grunt.data.archieml = {};
 
-    var files = grunt.file.expand("data/*.txt");
+    var files = grunt.file.expand("src/js/data/*.txt");
 
     files.forEach(function(f) {
       var name = path.basename(f).replace(/(\.docs)?\.txt$/, "");
       var contents = grunt.file.read(f);
-
-      var parsed = betty.parse(contents, {
-        onFieldName: t => t[0].toLowerCase() + t.slice(1)
-      });
+      var parsed = archieml.load(contents);
       grunt.data.archieml[name] = parsed;
     });
 
